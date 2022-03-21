668b27e262e2193f1205a545e076e9a6b55bf4b1
Binod Mishra <102038754+binodmishra2003@users.noreply.github.com>
2022-03-21 19:35:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function sendMoney(){
+    var enterName = document.getElementById("enterName").value;
+    var enterAmount = parseInt(document.getElementById("enterAmount").value);
+ var enterSName = document.getElementById("enterSName").value;
+  var findSenderBankAccount = enterSName + "BankBalance";
+    var enterSAmount =parseInt(document.getElementById(findSenderBankAccount).innerHTML);
+    if (enterAmount > enterSAmount) {
+       alert("Insufficient Balance.")
+    }
+    else {
+      var findUserBankAccount = enterName + "BankBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com.`)
+
+      // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
